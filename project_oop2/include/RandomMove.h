7f23fd4db29eb3c7cc7f7305c46d173e81891b56
PeninaSchuss <pninast@edu.hac.ac.il>
2022-06-08 00:48:21 +0300

    asdfghj

--- project_oop2/include/RandomMove.h
+++ project_oop2/include/RandomMove.h
@@ -8,10 +8,13 @@ public:
 	RandomMove(sf::Vector2i pos, std::unique_ptr<CanMoveToPlace> placeToMove);
 	~RandomMove() {}
 	virtual sf::Vector2f move(Board& b) override; 
+	void moveDifferently();//יכול להיות שזה יהיה observer
+
 private:
 	static bool m_register;
 	int m_directionEnum = rand() % 4;
 	sf::Vector2i m_direction={0,0};
-	int m_cx= 6 - rand() % 12, m_cy= 6 - rand() % 12;;
+	//int m_cx= 6 - rand() % 12, m_cy= 6 - rand() % 12;
+	sf::Clock m_clockForMove;
 };
 bool RandomMove::m_register = EnemyFactory::registerMove(RANDOM_MOVE, [](sf::Vector2i pos, std::unique_ptr<CanMoveToPlace> p)->std::shared_ptr<Move> { return std::make_shared<RandomMove>(pos, std::move(p)); });
--- project_oop2/include/TerritoryEater.h
+++ project_oop2/include/TerritoryEater.h
@@ -5,7 +5,9 @@ class Level;
 class TerritoryEater : public Enemies
 {
 public:
-	TerritoryEater(sf::Texture& t, sf::Vector2f f1, std::shared_ptr<Move> m, Level* l):Enemies(t,f1,m,l){}
+	TerritoryEater(sf::Texture& t, sf::Vector2f f1, std::shared_ptr<Move> m, Level* l) :Enemies(t, f1, m, l) {
+	
+	}
 	~TerritoryEater() {}
 	virtual void move(Board& b) override;
 private:
