54dd8f797cf27a15526c09e4ad970fb071cd72e2
MichalMeshi <96175315+MichalMeshi@users.noreply.github.com>
2022-06-16 00:02:45 +0300

    dfghj

--- project_oop2/include/GameController.h
+++ project_oop2/include/GameController.h
@@ -4,10 +4,17 @@ class InfoOfLevel;
 class GameController
 {
 public:
-	GameController(){}
+	GameController():m_winGame(Graphics::getGraphics().getTexture(WIN_GAME), sf::Vector2f(800, 500), sf::Vector2f(800, 500)),
+		m_gameOver(Graphics::getGraphics().getTexture(GAME_OVER), sf::Vector2f(800, 500), sf::Vector2f(500, 150))
+	{
+		m_winGame.setOrigin(400,250);
+		m_gameOver.setOrigin(250,75);
+	}
 	~GameController() {}
 	void runTheGame();
 private:
 	sf::RenderWindow m_window;
 	InfoOfLevel m_infoOfLevel;
+	Display m_winGame;
+	Display m_gameOver;
 };
--- project_oop2/src/GameController.cpp
+++ project_oop2/src/GameController.cpp
@@ -4,40 +4,37 @@ void GameController::runTheGame()
 {
 	enum EndOfLevelCondition endLevelCondition;
 	int curentLevel = 1;
-	while (curentLevel <= LEVELS_AMOUNT )   //לטפל בזה...
+	while (curentLevel <= LEVELS_AMOUNT)
 	{
 		m_window.create(sf::VideoMode({ WIDTH_WINDOW,HIGTH_WINDOW }), "Xonix");
-	//	m_window.setFramerateLimit(FRAME_LIMIT_SPEED);
-		//m_nameFile[LEVEL_NUMBER_INDEX] = char(curentLevel + 48);//מחשב בקלות את מספר השלב הנוכחי על פי קוד אסקי
-		Level level(m_window, curentLevel, m_infoOfLevel.m_amountEnemiesInLevelMatrix[curentLevel]); //בשביל הסרגל של השלב
+		Level level(m_window, curentLevel, m_infoOfLevel.m_amountEnemiesInLevelMatrix[curentLevel]); 
 		endLevelCondition = level.runLevel();
 		Graphics::getGraphics().getSoundVec()[CLOCK_SOUND]->pause();
 
 		if (endLevelCondition == FAIL_LEVEL)
-		{
-			//m_window.close();להעביר לבסוף
 			break;
-		}
-
 		else if (endLevelCondition == CLOSE)
 			return;
 		else if (endLevelCondition == FINISHLEVEL)
 		{
 			Graphics::getGraphics().getSoundVec()[WIN_SOUND]->play();
-
 			sf::sleep(sf::Time(sf::seconds(3)));
 			curentLevel++;
 		}
-
 	}
 
-	//if(curentLevel<4)
-	//	//תמונה של כשלון
-	/*else*/ if(curentLevel==4)
-		m_window.close();
+	if (curentLevel < 4)
+	{
+		m_gameOver.draw(m_window);
+		m_window.display();
+		sf::sleep(sf::Time(sf::seconds(3)));
+	}
+	else if (curentLevel == 4)
+	{
+		m_winGame.draw(m_window);
+		m_window.display();
+		sf::sleep(sf::Time(sf::seconds(3)));
+	}
 
-	//	//תמונה של ניצחון
-
-	//בכל מקרה, סגירת חלון
 	m_window.close();
 }
