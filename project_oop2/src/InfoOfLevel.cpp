0457de19ab80167965ccde79ffc322d2b7f4071c
PeninaSchuss <pninast@edu.hac.ac.il>
2022-06-17 01:03:43 +0300

    דגכעי

--- project_oop2/src/InfoOfLevel.cpp
+++ project_oop2/src/InfoOfLevel.cpp
@@ -11,9 +11,9 @@ InfoOfLevel::InfoOfLevel()
 	m_amountEnemiesInLevelMatrix[1].emplace_back(0);//SMART_MONSTER
 	m_amountEnemiesInLevelMatrix[2].emplace_back(0);
 	m_amountEnemiesInLevelMatrix[3].emplace_back(1);
-	m_amountEnemiesInLevelMatrix[1].emplace_back(10);//PERCENTAGE
-	m_amountEnemiesInLevelMatrix[2].emplace_back(10);
-	m_amountEnemiesInLevelMatrix[3].emplace_back(10);
+	m_amountEnemiesInLevelMatrix[1].emplace_back(75);//PERCENTAGE
+	m_amountEnemiesInLevelMatrix[2].emplace_back(80);
+	m_amountEnemiesInLevelMatrix[3].emplace_back(90);
 	m_amountEnemiesInLevelMatrix[1].emplace_back(3);//LIFE_AMOUNT
 	m_amountEnemiesInLevelMatrix[2].emplace_back(3);
 	m_amountEnemiesInLevelMatrix[3].emplace_back(3);
